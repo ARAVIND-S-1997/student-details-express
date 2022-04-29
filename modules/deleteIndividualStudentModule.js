31fe5720f75b826be262ab19ea11ef56b6fac4b0
ARAVIND-S-1997 <itsmearavind4@gmail.com>
2022-04-29 16:20:24 +0530

    delete students completed

--- modules/deleteIndividualStudentModule.js
+++ modules/deleteIndividualStudentModule.js
@@ -2,24 +2,29 @@
 import { createclassroom } from "../models/createClassroomModel.js";
 import jwt from "jsonwebtoken";
 
-export const deletestudentFunction=async(request,response)=>{
-    try{
-        const{token,emailid,classroomid}=request.headers;
-        console.log("Token (Delete student function):",token);
+
+export const deletestudentFunction = async (request, response) => {
+    try {
+        const { token, emailid, classroomid } = request.headers;
+        console.log("Token (Delete student function):", token);
         console.log(emailid);
-        console.log(classroomid);
-        const{id}=request.params;
-        console.log("Id (Delete student function) :",id);
+        // console.log(classroomid);
+        const { id } = request.params;
+        console.log("Id (Delete student function) :", id);
 
-        const check=jwt.verify(token,process.env.Secret_key);
-        if(check){
-            const deleteReq=await createclassroom.updateOne({},{$pull:{students:{_id:id}}});
-            }
-            else{
-                response.status(400).send("Ivalid token")
-            }
-
-    }catch(error){
-        console.log("Error is (Delete student function):",error);
+        const check = jwt.verify(token, process.env.Secret_key);
+        if (check) {
+            const deleteReq = await createclassroom.updateMany({ user:emailid }, { $pull: { students: { _id: id } } });
+        
+            if (deleteReq) {
+                const findreq = await createclassroom.findOne({ _id: id });
+                    response.status(200).send(findreq)
+                }
+        }
+        else {
+            response.status(400).send("Ivalid token")
+        }
+    } catch (error) {
+        console.log("Error is (Delete student function):", error);
     }
 }
